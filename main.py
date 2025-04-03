19eb0cd7d31e511f1db7706e8cff98e696a38e73
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-03 18:24:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import random
 from turtle import Turtle, Screen
-turtle = Turtle()
+from random import Random
 #trying out things
+
+turtle = Turtle()
+colors = ["red","blue","green","yellow","pink","orange","purple","cyan","grey","brown"]
+shape = 3
+while shape<=10:
+    shape_degrees = 360/shape
+    random_color = random.choice(colors)
+    turtle.color(random_color)
+    for side in range(shape):
+        turtle.forward(100)
+        turtle.right(shape_degrees)
+    shape+=1
+
 
 
 
