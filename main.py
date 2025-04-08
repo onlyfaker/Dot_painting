c96407d5a9de289a929d883ae8bceabfc43b6258
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-07 23:54:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from turtle import Turtle, Screen
+import turtle as t
 # Playing around with turtle
 
 # Setup
@@ -8,8 +9,14 @@ turtle.ht()
 screen = Screen()
 screen.setup(width=1700, height=850)
 turtle.speed(0)  # No animation delay
+t.colormode(255)
 
-colors = ["red", "blue", "green", "yellow", "pink", "orange", "purple", "cyan", "grey", "brown", "black", "brown"]
+#funciton for random RGB colors and not just a list of colors
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return turtle.pencolor(r,g,b)
 
 # Move the turtle to the left before drawing shapes
 turtle.penup()
@@ -20,8 +27,9 @@ turtle.pendown()
 shape = 3
 while shape <= 10:
     shape_degrees = 360 / shape
-    random_color = random.choice(colors)
-    turtle.color(random_color)
+    # random_color = random.choice(colors)
+    # turtle.color(random_color)
+    random_color()
     for side in range(shape):
         turtle.forward(100)
         turtle.right(shape_degrees)
@@ -34,9 +42,9 @@ turtle.pendown()
 
 for a in range(100):
     random_direction = random.randint(1, 3)
-    random_color = random.choice(colors)
-
-    turtle.color(random_color)
+    # random_color = random.choice(colors)
+    # turtle.color(random_color)
+    random_color()
     turtle.pensize(8)
 
     if random_direction == 1:
