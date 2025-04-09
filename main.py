addd8759d5af72803c841df68c70e0339c855c7d
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-08 23:38:29 -0400

    testing

--- main.py
+++ main.py
@@ -68,24 +68,35 @@ def random_color():
 # #------------------------ x x x x ----------------------------
 
 #Final valuable painting
+#
+# colors = colorgram.extract("painting.jpg",40)
+# rgb_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b=  color.rgb.b
+#
+#     color_tuple = (r,g,b)
+#     rgb_colors.append(color_tuple)
+#
+# print(rgb_colors)
+color_list_from_image =   [(198, 13, 32), (248, 236, 25), (40, 76, 188), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51), (6, 68, 42), (176, 176, 233), (239, 168, 161), (249, 8, 48), (5, 246, 222), (15, 76, 110), (243, 15, 14), (38, 43, 221)]
 
-colors = colorgram.extract("painting.jpg",40)
-rgb_colors = []
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b=  color.rgb.b
+turtle.penup()
+turtle.goto(250, -150)
 
-    color_tuple = (r,g,b)
-    rgb_colors.append(color_tuple)
-    
-print(rgb_colors)
-color_list =   [(198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51), (6, 68, 42), (176, 176, 233), (239, 168, 161), (249, 8, 48), (5, 246, 222), (15, 76, 110), (243, 15, 14), (38, 43, 221)]
+def drawing_painting():
+    vertical_pos = -150
+    for a in range(0,3):
+        for b in range(0,10):
+            random_color = random.choice(color_list_from_image)
+            turtle.fd(50)
+            turtle.dot(20,random_color)
 
-    
-    
-    
-    
-    
-    
+        vertical_pos += 50
+        turtle.goto(250, vertical_pos)
+
+drawing_painting()
+
+turtle.pendown()
 screen.exitonclick()
